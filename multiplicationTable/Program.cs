6eb054229103acffe019fbd3922b21a4c0837599
Magda <magdagvirjishvili@gmail.com>
2023-07-10 00:35:36 +0400

    magdas_commit

---
+++ multiplicationTable/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("enter the number ");
+        int num = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("multiplication table " + "on " + num);
+
+
+        for (int i = 0; i <=10; i++)
+        {
+            Console.WriteLine(num + "*" + i + "=" + (num * i));
+        }
+    }
+}
